3f90e1a78d51bc7b97f5f8bebba6ccba5d5b64aa
OrangeKim04 <queemkimu@naver.com>
2025-03-25 02:12:23 +0900

    [feat]파일 절대경로 설정

--- ZeroPick/src/App.tsx
+++ ZeroPick/src/App.tsx
@@ -1,13 +1,13 @@
 import styled from 'styled-components';
 function App() {
-  return (
-    <>
-      <Text>제로픽</Text>
-    </>
-  );
+   return (
+      <>
+         <Text>제로픽</Text>
+      </>
+   );
 }
 
 export default App;
 const Text = styled.p`
-  color: blue;
+   color: blue;
 `;
