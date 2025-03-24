b05f732b04ef88287b021cba40787fdd155f63eb
OrangeKim04 <queemkimu@naver.com>
2025-03-25 03:40:31 +0900

    [feat]로그인 페이지 UI 구현

--- ZeroPick/src/App.tsx
+++ ZeroPick/src/App.tsx
@@ -1,9 +1,12 @@
+import '@/font.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+
 function App() {
    return (
-      <>
+      <Router>
          <LoginPage />
-      </>
+      </Router>
    );
 }
 
--- ZeroPick/src/pages/LoginPage.tsx
+++ ZeroPick/src/pages/LoginPage.tsx
@@ -1,8 +1,71 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 const LoginPage = () => {
    return (
-      <>
-         <p>로그인 페이지</p>
-      </>
+      <Container>
+         <Box>
+            <Text>로그인</Text>
+            <Form>
+               <InputBox>
+                  <Input placeholder="아이디" />
+                  <Input placeholder="비밀번호" />
+               </InputBox>
+               <Button>로그인</Button>
+            </Form>
+            <SubText to="">회원가입</SubText>
+         </Box>
+      </Container>
    );
 };
 export default LoginPage;
+const Input = styled.input`
+   width: 80%;
+   height: 3.2rem;
+   border-color: #ff005c;
+   border-radius: 10px;
+   box-sizing: border-box;
+   padding-left: 15px;
+   outline: none;
+`;
+const Container = styled.div``;
+const InputBox = styled.div`
+   display: flex;
+   width: 100%;
+   flex-direction: column;
+   align-items: center;
+   gap: 10px;
+`;
+const Text = styled.p`
+   font-family: Bold;
+   font-size: 2rem;
+`;
+const Button = styled.button`
+   width: 80%;
+   background-color: #ff005c;
+   height: 3.2rem;
+   border: none;
+   border-radius: 10px;
+   color: white;
+   font-size: 1.2rem;
+   font-family: SemiBold;
+`;
+const Form = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   gap: 40px;
+   width: 100%;
+   margin-top: 20px;
+`;
+const SubText = styled(Link)`
+   font-family: Regular;
+   margin-top: 15px;
+`;
+const Box = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   width: 100%;
+   position: absolute;
+   bottom: 200px;
+`;
