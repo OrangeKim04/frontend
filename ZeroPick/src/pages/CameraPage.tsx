4b0c3a7e38d8db8df87003c452595bb9ab7303b1
OrangeKim04 <queemkimu@naver.com>
2025-03-27 19:28:50 +0900

    fix: build 오류 수정

--- ZeroPick/src/pages/CameraPage.tsx
+++ ZeroPick/src/pages/CameraPage.tsx
@@ -1,7 +1,37 @@
-import React, { useState, useRef } from 'react';
+import { useState, useRef } from 'react';
 import { Camera, CameraType } from 'react-camera-pro';
 import styled from 'styled-components';
 import icon from '@/assets/camera_button.svg';
+
+const CameraPage = () => {
+   const [image, setImage] = useState<string | null>(null);
+   const camera = useRef<CameraType>(null);
+
+   return (
+      <Wrapper>
+         <Camera ref={camera} aspectRatio="cover" errorMessages={{}} />
+         <Control>
+            {image && <img src={image} alt="Captured" />}
+
+            <TakePhotoButton
+               src={icon}
+               onClick={() => {
+                  if (camera.current) {
+                     const photo = camera.current.takePhoto();
+                     if (typeof photo === 'string') {
+                        setImage(photo); // photo가 string일 경우에만 setImage 호출
+                     } else {
+                        console.error('Photo is not a string');
+                     }
+                  }
+               }}
+            />
+         </Control>
+      </Wrapper>
+   );
+};
+
+export default CameraPage;
 const Wrapper = styled.div`
    position: fixed;
    width: 100%;
@@ -24,33 +54,3 @@ const Control = styled.div`
 const TakePhotoButton = styled.img`
    width: 80px;
 `;
-
-const CameraPage = () => {
-   const [numberOfCameras, setNumberOfCameras] = useState(0);
-   const [image, setImage] = useState<string | null>(null);
-   const camera = useRef<CameraType>(null);
-
-   return (
-      <Wrapper>
-         <Camera
-            ref={camera}
-            aspectRatio="cover"
-            numberOfCamerasCallback={setNumberOfCameras}
-         />
-         <Control>
-            <TakePhotoButton
-               src={icon}
-               onClick={() => {
-                  if (camera.current) {
-                     const photo = camera.current.takePhoto();
-                     console.log(photo);
-                     setImage(photo);
-                  }
-               }}
-            />
-         </Control>
-      </Wrapper>
-   );
-};
-
-export default CameraPage;
